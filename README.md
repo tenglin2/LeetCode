ec6b1e21fa15d098a955cdba7049b41ad773e356
Teng Lin <tenglin2@gmail.com>
2019-05-30 21:33:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a compilation of the code I used to solve LeetCode challenges. I will primarily be using JavaScript with ES6+ syntax.
---
+++ sandbox.js
@@ -0,0 +1,5 @@
+console.log('do i need npm?');
+
+// No I don't. I can still run node because it's on my local computer. The only reason to do npm init is for packages and to start a node server.
+
+// This LeetCode folder is the intention of adding all the coding I do on leetcode to store as solutions. Mainly so that I can look back and reason through the logic.

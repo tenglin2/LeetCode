12abb2446c7e05a4a5d7468d2348e57236fc83c7
Teng Lin <tenglin2@gmail.com>
2019-06-30 14:17:31 -0400

    Added official solutions

--- IsSubsequence.js
+++ IsSubsequence.js
@@ -48,3 +48,25 @@ const isSubsequence = function(firstString, secondString) {
 
 isSubsequence('abce', 'abzce');
 console.log(isSubsequence('abce', 'abzce'));
+
+// Official Solution - Iterative, same logic as above but cleaner.
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// Official Solution - Recursive
+// Mutates the strings using slice and continues until one of the strings is empty. Actually pretty clever.
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}

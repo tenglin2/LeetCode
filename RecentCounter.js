d56e2c6db9f39e80afb1fd6e99a8dfc49037ed21
Teng Lin <tenglin2@gmail.com>
2019-07-14 12:52:58 -0400

    recent counter counts number of pings with a queue.

---
+++ RecentCounter.js
@@ -0,0 +1,3 @@
+/**
+ * Write a class called RecentCounter to count the recent requests. Give it one method called ping which takes a parameter int t, where t represents time in milliseconds. The method will return the number of pings made 3000 ms ago until now. These bounds are inclusive at we should count the current ping as well.
+ */

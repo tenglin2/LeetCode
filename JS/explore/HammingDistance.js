8f0ecc4bf56990dbbb43939df9c62a95780fc89d
Teng Lin <tenglin2@gmail.com>
2020-01-09 00:18:12 -0500

    hamming distance using xor and then iterating through each bit with AND and logical right shift.

---
+++ JS/explore/HammingDistance.js
@@ -0,0 +1,18 @@
+/**
+ * Hamming distance is the number of positions where the bits differ.
+ * I would do a XOR operation since all the like terms will become 0. Then iterate through each bit and check if the value is 1 using & 1.
+ * 
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const hammingDistance = function(x, y) {
+	let num = x ^ y;
+	let oneCount = 0;
+	while (num !== 0) {
+		if (num & (1 === 1)) oneCount += 1;
+		num = num >>> 1;
+	}
+
+	return oneCount;
+};
